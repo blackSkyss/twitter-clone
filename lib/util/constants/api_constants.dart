4e0de1cc7c49f5e1d02b3bcb87363b11c824752a
blackSkyss <blacksky01102001@gmail.com>
2023-07-26 18:04:51 +0700

    change redirecturl

--- lib/util/constants/api_constants.dart
+++ lib/util/constants/api_constants.dart
@@ -1,6 +1,5 @@
 class APIConstants {
   static const String baseUrl = 'https://bmos-system.azurewebsites.net/odata';
-  static const String redirectUrl =
-      'https://payment-aae40.web.app/payment-result/';
+  static const String redirectUrl = 'https://payment-aae40.web.app';
   static const String contentType = 'application/json';
 }
